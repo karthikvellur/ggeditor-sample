bfe16478999e068cf0225f0bca603dced4d53fa2
kvellur <karthik_vellur@intuit.com>
2020-11-22 10:40:07 +0530

    added ggeditor dependencies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import GGEditor, { Flow, EditableLabel } from "gg-editor";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "./styles.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const data = {
+    nodes: [
+        {
+            id: "0",
+            label: "Node",
+            x: 55,
+            y: 55
+        },
+        {
+            id: "1",
+            label: "Node",
+            x: 55,
+            y: 255
+        }
+    ],
+    edges: [
+        {
+            label: "Label",
+            source: "0",
+            target: "1"
+        }
+    ]
+};
+
+function App() {
+    return (
+        <GGEditor className="editor">
+            <Flow
+                className="editorBd"
+                data={data}
+                graphConfig={{
+                    defaultNode: {
+                        shape: "bizFlowNode"
+                    },
+                    defaultEdge: {
+                        shape: "bizFlowEdge"
+                    }
+                }}
+            />
+            <EditableLabel />
+        </GGEditor>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
